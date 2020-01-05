e27926c7235508b5a6b5d1a3f43f23f3f081f8ed
Jacob Robinson <95jacob07@gmail.com>
2020-01-04 23:30:56 -0800

    Using Heroku port env variable

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(MONGO_URI)
   .then(result => {
-    const server = app.listen(8000);
+    const server = app.listen(process.env.PORT || 8000);
     const io = require("./socket").init(server);
     io.on("connection", socket => {
       console.log("client connected");
